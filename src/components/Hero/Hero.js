d9e1b55cde089222c5fb437068198b3616a6065b
Xanh Vi <93765763+tranthanhvi@users.noreply.github.com>
2024-02-25 23:55:00 +0200

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
 import { Section, Button, SectionTitle, SectionText} from '../../styles/GlobalComponents';
-import { LeftSection } from './HeroStyles';
+import { HeroButton, LeftSection } from './HeroStyles';
+import { Div3 } from '../Header/HeaderStyles';
+import { SocialIcons } from '../Header/HeaderStyles';
+import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from 'react-icons/ai';
 
 const Hero = () => (
-  <Section row nopadding>
+  <Section>
     <LeftSection>
       <SectionTitle main center>
         Hello, <br />
         I am Thanh
       </SectionTitle>
       <SectionText>
-        I like to read, love to cook, and ofc CODE TO LIVE!
+        I like to <HeroButton href='https://spangle-estimate-da8.notion.site/b88ede15872e49f69b26725bba1eb9d2?v=00583c37fe044f67817590df87e9fb59' target='_blank'>read</HeroButton>, love to <HeroButton href='https://tranthanhvi.github.io/cooking-receipe-website/'>cook</HeroButton>, and ofc <HeroButton href='https://github.com/tranthanhvi' target='_blank'>CODE</HeroButton> TO LIVE!
       </SectionText>
       <Button onClick={() => window.location = 'https://github.com/tranthanhvi'}>Reach me</Button>
+      <Div3 hide>
+        <SocialIcons href='https://github.com/tranthanhvi'>
+          <AiFillGithub size='2rem' />
+        </SocialIcons>
+        <SocialIcons href='https://www.linkedin.com/in/tran-thanh-2900b31a0/'>
+          <AiFillLinkedin size='2rem' />
+        </SocialIcons>
+        <SocialIcons href='https://www.instagram.com/xanhvitran/'>
+          <AiFillInstagram size='2rem' />
+        </SocialIcons>
+      </Div3>
     </LeftSection>
   </Section >
 );
