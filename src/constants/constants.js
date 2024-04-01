66b3cb63058e7a2dc3211152f7f2bb9039faf3df
Thanh Tran <tranthanhvi66@gmail.com>
2024-04-01 14:41:10 +0300

    updated links

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -13,6 +13,7 @@ export const projects = [
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
     image: 'images/2.jpg',
     tags: ['React', 'JavaScript'],
+    source: 'https://bit.ly/3TxLy36',
     visit: 'https://www.figma.com/proto/2CxmhhiXbGf5RZkZ5lxSFT/Project-Management?node-id=58-6&starting-point-node-id=58%3A6',
     id: 1,
   },
